4b397f486a1829f73468ebe8f46f0d875f35929d
Marcia Kimenyembo <mkk0963@mavs.uta.edu>
2023-01-28 01:50:46 -0600

    changed the values of age

--- P02/bonus/MyPets.java
+++ P02/bonus/MyPets.java
@@ -3,10 +3,10 @@ public class MyPets
     public static void main(String[] args)
     {
         Pet[] pet = new Pet[4];
-        pet[0] = new Pet("Mokonzi", 5.2, Type.Okapi);
+        pet[0] = new Pet("Mokonzi", 5, Type.Okapi);
         pet[1] = new Pet("Motema", 6, Type.Monkey);
-        pet[2] = new Pet("Mboka", 2.5, Type.Snake);
-        pet[3] = new Pet("Congo", 3.8, Type.Leopard);
+        pet[2] = new Pet("Mboka", 2, Type.Snake);
+        pet[3] = new Pet("Congo", 3, Type.Leopard);
 
         for(Pet index : pet)
         {
