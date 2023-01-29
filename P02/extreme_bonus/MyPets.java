ff2765305ca666014c3852c17d9f46076aa21979
Marcia Kimenyembo <mkk0963@mavs.uta.edu>
2023-01-28 18:15:33 -0600

    final extreme bonus

--- P02/extreme_bonus/MyPets.java
+++ P02/extreme_bonus/MyPets.java
@@ -1,33 +1,47 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MyPets
 {
     public static void main(String[] args)
     {
-        //ArrayList<Pet> pet = new ArrayList<Pet>();
-
+        ArrayList<Pet> pet = new ArrayList<Pet>(1);
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter your pet's name: ");
-        String name = in.nextLine();
+        System.out.print("Would you like to add a pet? press y(yes) to add OR n(no) to quit: ");
+        char yes = in.nextLine().toLowerCase().charAt(0);
 
-        System.out.print("What kind of pet is it? ");
-        String kind = in.nextLine();
-        Type type = Type.valueOf(kind.substring(0,1).toUpperCase() + kind.substring(1));
+        while(yes == 'y')
+        {
+            System.out.println();
+            System.out.print("Enter your pet's name: ");
+            String name = in.nextLine();
+    
+            System.out.println();
+            System.out.print("What kind of pet is it? Snake, Leopard, Monkey, or Okapi only: ");
+            String kind = in.nextLine();
+            Type type = Type.valueOf(kind.substring(0,1).toUpperCase() + kind.substring(1).toLowerCase());
+    
+            System.out.println();
+            System.out.print("Enter your pet's age: ");
+            String tempAge = in.nextLine();
+            double age = Double.valueOf(tempAge);
 
-        System.out.print("Enter your pet's age: ");
-        double age = in.nextDouble();
 
-        Pet[] pet = new Pet[4];
-        pet[0] = new Pet(name, age, type);
-        pet[1] = new Pet("Motema", 6, Type.Monkey);
-        pet[2] = new Pet("Mboka", 2, Type.Snake);
-        pet[3] = new Pet("Congo", 3, Type.Leopard);
+            pet.add(new Pet(name, age, type));
 
+            System.out.println();
+            System.out.print("press y(yes) to add another pet OR n(no) to quit: ");
+            yes = in.nextLine().toLowerCase().charAt(0);
+        }
+
+        System.out.println();
         for(Pet index : pet)
         {
             System.out.println(index);
         }
+
+        System.out.println();
+        System.out.printf("The size of the array is %d", pet.size());
     }
 }
