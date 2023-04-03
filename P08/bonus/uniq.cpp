287608dfa2101983a9d161c3e24d1a3bc4bb5709
Marcia Kimenyembo <mkk0963@mavs.uta.edu>
2023-04-03 00:22:23 -0500

    finish bonus

---
+++ P08/bonus/uniq.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//#include <string>
+
+int main(int argc, char* argv[]) 
+{
+    std::string previous;
+
+    for (int index = 1; index < argc; index++) 
+    {
+        std::string current(argv[index]);
+
+        if (current != previous) 
+        {
+            std::cout << current << " ";
+            previous = current;
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
